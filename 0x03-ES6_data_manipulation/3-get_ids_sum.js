12bc41056a832c5918b00827e4f084ed38e572a2
Haile-08 <haile4cmd@gmail.com>
2024-01-11 02:00:24 -0800

    Use the reduce function on the array

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (accumulator, currentValue) => accumulator.id || accumulator + currentValue.id,
+      0,
+    );
+  }
+  return 0;
+}
